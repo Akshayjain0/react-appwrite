5d20135f1a03b0f359837edbef7f8bfe67cd6891
Akshayjain0 <101654081+Akshayjain0@users.noreply.github.com>
2024-01-12 15:53:39 +0530

    Appwrite and project setup completed

---
+++ blogWithAppwrite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import conf from './conf/conf'
+
+function App() {
+  console.log(conf.appwriteProjectId)
+
+  return (
+    <>
+    <h1>Blog with appwrite</h1>      
+    </>
+  )
+}
+
+export default App
